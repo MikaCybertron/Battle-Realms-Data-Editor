c70f41bb29a11bc17b465260b74ece64056ad47f
Mika Cybertron <saydannaydan2000@gmail.com>
2024-05-15 04:04:26 +0800

    Sync All Codes to Projects

--- Battle Realms Data Editor/Battle Realms Data Editor/Properties/AssemblyInfo.cs
+++ Battle Realms Data Editor/Battle Realms Data Editor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("New Battle Realms Data Editor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Battle Realms Data Editor")]
+[assembly: AssemblyDescription("Battle Realms Data Editor by Mika Cybertron")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("New Battle Realms Data Editor")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © Mika Cybertron 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
