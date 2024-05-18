6ca79b69cb42d4fabb85a209379ba4f23e635fb0
Kurt09 <kiratisonic555@gmail.com>
2024-05-18 12:08:38 +0700

    Add files via upload

---
+++ Battle Realms Data Editor/Battle Realms Data Editor/WindowsAPI.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleRealmsDataEditor
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class WindowsAPI
+    {
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+    }
+}
